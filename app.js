e2ab1abeb7c8d20eaecd1b071d3b4f1b8f1d718a
wego11ya <wego11ya@gmail.com>
2023-02-25 23:02:01 +0800

    add express-session and login feature via passport

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const routes = require('./routes')
+const session = require('express-session')
+const usePassport = require("./config/passport");
 const app = express()
 const PORT = 3000
 // handlebars設定
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // 使用body-parser
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
@@ -9,9 +10,13 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.send('login')
-})
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+)
 
 router.get('/register', (req, res) => {
   res.render('register')
