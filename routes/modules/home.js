1d2fc92c316923982e68c8315ecc31f0cb66e111
wego11ya <wego11ya@gmail.com>
2023-02-25 13:22:19 +0800

    add home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const { Todo } = require("../../models");
 
 router.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { 
+      return res.render('index', { todos: todos }) 
+    })
+    .catch((error) => { 
+      return res.status(422).json(error) 
+    })
 })
 
 module.exports = router
