a2b7097bd5309df13fc13e5faa90df1ae3ff7d5c
wego11ya <wego11ya@gmail.com>
2023-02-25 11:16:07 +0800

    add default seeder

---
+++ seeders/20230225014747-default-data.js
@@ -0,0 +1,34 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: bcrypt.hashSync(SEED_USER.password, bcrypt.genSaltSync(10), null),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+      .then(userId => queryInterface.bulkInsert('Todos',
+        Array.from({ length: 10 }).map((_, i) =>
+          ({
+            name: `name-${i}`,
+            UserId: userId,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          })
+        ),))
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Todos', null, {})
+    .then(() => queryInterface.bulkDelete('Users', null, {}))
+  }
+};
