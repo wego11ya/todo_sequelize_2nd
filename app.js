ee85041b264f6199680892edbb6e0b94cf5d0509
wego11ya <wego11ya@gmail.com>
2023-02-25 13:48:02 +0800

    add detail page

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 // 引入模組程式碼
 const home = require('./modules/home')
 const users = require('./modules/users')
+const todos = require('./modules/todos')
 
 // 引入路由模組，將網址結構導向符合/字串的request導向對應的模組
 router.use('/users', users)
+router.use('/todos', todos)
 router.use('/', home)
 
 module.exports = router
