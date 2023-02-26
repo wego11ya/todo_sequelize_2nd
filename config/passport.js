50928153f3b40fc2d09e3f22e6e9a67c3ea748b8
wego11ya <wego11ya@gmail.com>
2023-02-26 13:25:05 +0800

    add warning_msg in config/passport.js

--- config/passport.js
+++ config/passport.js
@@ -6,15 +6,19 @@ const User = db.User
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ 
+    usernameField: 'email',
+    passReqToCallback: true
+  }, 
+  (req, email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, req.flash('warning_msg', 'That mail is not registered!'))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, req.flash('warning_msg', 'Email or Password incorrect.'))
           }
           return done(null, user)
         })
