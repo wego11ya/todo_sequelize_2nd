83f66ca4fb1a7ea62279862953227311b6e06eee
wego11ya <wego11ya@gmail.com>
2023-02-26 14:03:58 +0800

    add relations between User and Todo Models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const { Todo } = require("../../models");
+const { User } = require("../../models");
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { 
-      return res.render('index', { todos: todos }) 
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if(!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id}
+      })
     })
-    .catch((error) => { 
-      return res.status(422).json(error) 
+    .then((todos) => {
+      res.render('index', {todos})
     })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const { Todo } = require("../../models");
 
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
-    .then((todo) => {
-      console.log(todo)
+  return Todo.findOne({
+    where: {id, UserId}
+  })
+    .then((todo) => {      
       res.render('detail', { todo: todo.toJSON() })})
     .catch(error => console.log(error))
 })
