ae05787de9d721af292c4c1401eea040a54c45f3
wego11ya <wego11ya@gmail.com>
2023-02-26 16:40:14 +0800

    add facebook login functionality and remove fb secret key

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
@@ -6,13 +9,14 @@ const routes = require('./routes')
 const session = require('express-session')
 const usePassport = require("./config/passport");
 const flash = require('connect-flash')
+
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 // handlebars設定
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
 
 // 引入路由模組，將網址結構導向符合/字串的request導向對應的模組
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
