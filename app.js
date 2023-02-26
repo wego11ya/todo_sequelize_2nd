dc6a78eab20cea706b472cb245a380f70d437277
wego11ya <wego11ya@gmail.com>
2023-02-26 12:03:49 +0800

    add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
