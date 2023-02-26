ea2712b36f3aa9951c0643cad39728f7b4773b2b
wego11ya <wego11ya@gmail.com>
2023-02-26 12:39:33 +0800

    add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs')
 const routes = require('./routes')
 const session = require('express-session')
 const usePassport = require("./config/passport");
+const flash = require('connect-flash')
 const app = express()
 const PORT = 3000
 // handlebars設定
@@ -19,10 +20,13 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
